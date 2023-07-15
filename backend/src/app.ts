a98cd02fa9d2055b7b7bab78ea7fc6485862548d
lizonkisel <ees.hwork@gmail.com>
2023-07-15 14:05:13 +0300

    hotfix: fix cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
@@ -15,6 +15,9 @@ mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
 // app.use(cors())
+app.use(cors({
+  origin: ['http://lizonkisel.students.nomoreparties.sbs', 'https://lizonkisel.students.nomoreparties.sbs'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
